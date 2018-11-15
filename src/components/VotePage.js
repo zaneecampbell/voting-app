5b57923d9eafbe0248d9ce01ba8abffc311fe5c2
Zane Campbell <zanegitinit@gmail.com>
2018-11-14 21:01:36 -0800

    voting page prepped for connecting to firebase

--- src/components/VotePage.js
+++ src/components/VotePage.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class CreatePage extends React.Component = (props) => (
-  <div>
-    hi
-    <p>{props.question}</p>
-  </div>
-);
+class VotePage extends React.Component {
+  state = {
+    id: ''
+  }
+
+  handleCheat = () => {
+  };
+
+  componentWillMount() {
+    const id = window.location.href
+    this.setState(() => ({ id }))
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.question}
+        <button onClick={this.handleCheat}>Cheaty Button</button>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
-    question: state.poll.optionsData.question,
-    id: state.poll.id
+    question: state.poll.optionsData.question
   }
 }
 
