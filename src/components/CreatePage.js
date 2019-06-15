6315a04bb6486729e6c8c131e8e3b951cb77aecd
Zane Campbell <zanegitinit@gmail.com>
2019-06-15 13:02:39 -0700

    fixed updating createpage form to something I understand kept legacy code in comment

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import database from '../firebase/firebase';
-import moment from 'moment';
+import moment, { updateLocale } from 'moment';
 import { startAddOptions } from '../actions/poll';
 import Header from './Header';
 import Paper from '@material-ui/core/Paper';
@@ -16,7 +16,7 @@ const styles = theme => ({
     fontSize: '50px',
     [theme.breakpoints.down('sm')]: {
       fontSize: '30px'
-  }
+    }
   },
   buttonLabel: {
     fontSize: '30px',
@@ -29,27 +29,41 @@ const styles = theme => ({
 // CreatePage starts here ----------------------------------------------------------------------
 class CreatePage extends React.Component {
   state = {
-    options: [{ option: '', count: 0 }, { option: '', count: 0 }, { option: '', count: 0 }],
+    options: [
+      { option: '', count: 0 },
+      { option: '', count: 0 },
+      { option: '', count: 0 }
+    ],
     list: [],
     question: ''
   };
 
   // so you can type in the question box
-  handleUpdateQuestion = (e) => {
-    const question = e.target.value
+  handleUpdateQuestion = e => {
+    const question = e.target.value;
     this.setState({ question });
-  }
+  };
 
   // if at the bottom of the input list automatically adds a new one when you start typing
-  handleAddOption = (e) => {
-    const id = e.target.id
-    const text = e.target.value
-    // I have no idea how this actually updates the textfield but it does, but seems you dont even need to concat text? Spent an 3 hours trying to understand
+  handleAddOption = e => {
+    const id = e.target.id;
+    const text = e.target.value;
+    // Makes a copy of the object in the array
+    let copy = this.state.options;
+    // updates the copy
+    copy[id].option = text;
+    // sets the copy as the new state
     this.setState({
-      ...this.state.options[id].option.concat(text)
+      options: copy
     });
 
-    this.state.options[id].option = text
+    // The code used to work this way I don't know why it worked this way so I eventually went back and fixed it. The above now works in a way I understand.
+    // I have no idea how this actually updates the textfield but it does, but seems you dont even need to concat text? Spent an 3 hours trying to understand
+    // this.setState({
+    //   ...this.state.options[id].option.concat(text)
+    // });
+
+    // this.state.options[id].option = text;
     // This below also works I have no idea why spreading it allows me to do this
 
     // this.setState({
@@ -59,21 +73,23 @@ class CreatePage extends React.Component {
     // End of not knowing how it works
 
     if (id == this.state.options.length - 1) {
-      this.setState({ options: this.state.options.concat([{ option: '', count: 0 }]) });
+      this.setState({
+        options: this.state.options.concat([{ option: '', count: 0 }])
+      });
     }
   };
 
   // Submits data to firebase and redux then takes you to the votepage
-  handleOnSubmit = (e) => {
+  handleOnSubmit = e => {
     e.preventDefault();
     const optionsArray = [];
     let number = 0;
-    const options = this.state.options.map((option) => {
+    const options = this.state.options.map(option => {
       // Removes blank answers from the choices then adds choices to an array
       if (option.option.trim() != '') {
         option.firebaseIndex = number;
         number = number + 1;
-        return optionsArray.push(option)
+        return optionsArray.push(option);
       } else {
         // don't add it to the array
       }
@@ -81,7 +97,7 @@ class CreatePage extends React.Component {
 
     // Makes it so you need at least 2 options and a question
     if (this.state.question === '' || optionsArray.length < 2) {
-      alert("Please add a question and at least 2 options")
+      alert('Please add a question and at least 2 options');
     } else {
       // Used for testing on one page
       this.setState({
@@ -105,64 +121,73 @@ class CreatePage extends React.Component {
     const { classes } = this.props;
 
     return (
-        <Grid
+      <Grid
         container
-        direction="column"
-        justify="space-between"
-        alignItems="center"
+        direction='column'
+        justify='space-between'
+        alignItems='center'
         spacing={0}
-        >
+      >
         <Grid item>
           <Header />
-        </ Grid>
-          <Grid item>
-            <Paper style={{padding: '10px'}}>
-              <form onSubmit={this.handleOnSubmit}>
-                <Input
-                  classes={{root: classes.textfield}}
-                  style={{marginTop: '50px', marginBottom: '50px'}}
-                  type='text'
-                  value={this.state.question}
-                  placeholder='Question here'
-                  autoComplete='off'
-                  onInput={this.handleUpdateQuestion}
-                />
-                <div id='input-container'>
-                  {/* Maps out option inputs dynamically as more are needed */}
-                  {this.state.options.map((option, idx) => (
-                    <div key={idx}>
-                      <Input
-                        classes={{root: classes.textfield}}
-                        id={`${idx}`}
-                        type='text'
-                        value={option.option}
-                        placeholder={`Option ${idx + 1}`}
-                        onInput={this.handleAddOption}
-                        autoComplete='off'
-                      />
-                    </div>
-                  ))}
-                </div>
-                <Button style={{marginTop: '15px'}} classes={{label: classes.buttonLabel}} type='submit'>Create</Button>
-              </form>
-            </Paper>
-          </Grid>
         </Grid>
-    )
-  }
-};
-
-const mapStateToProps = (state) => {
-  return {
-    id: state.poll.id
+        <Grid item>
+          <Paper style={{ padding: '10px' }}>
+            <form onSubmit={this.handleOnSubmit}>
+              <Input
+                classes={{ root: classes.textfield }}
+                style={{ marginTop: '50px', marginBottom: '50px' }}
+                type='text'
+                value={this.state.question}
+                placeholder='Question here'
+                autoComplete='off'
+                onInput={this.handleUpdateQuestion}
+              />
+              <div id='input-container'>
+                {/* Maps out option inputs dynamically as more are needed */}
+                {this.state.options.map((option, idx) => (
+                  <div key={idx}>
+                    <Input
+                      classes={{ root: classes.textfield }}
+                      id={`${idx}`}
+                      type='text'
+                      value={option.option}
+                      placeholder={`Option ${idx + 1}`}
+                      onInput={this.handleAddOption}
+                      autoComplete='off'
+                    />
+                  </div>
+                ))}
+              </div>
+              <Button
+                style={{ marginTop: '15px' }}
+                classes={{ label: classes.buttonLabel }}
+                type='submit'
+              >
+                Create
+              </Button>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  startAddOptions: (optionsData) => dispatch(startAddOptions(optionsData)),
+const mapStateToProps = state => {
+  return {
+    id: state.poll.id
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  startAddOptions: optionsData => dispatch(startAddOptions(optionsData))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CreatePage));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(CreatePage));
 
 // Done
 
